6836c0edcc28b6a375f2854c4c20e1ec69275b14
Vladislav Magomedov <vladdmagg94@yandex.ru>
2023-02-14 06:10:16 +0300

    refactor: update user table migration

--- src/db/migration/1676238451353-UserTableCreation.ts
+++ src/db/migration/1676238451353-UserTableCreation.ts
@@ -1,20 +1,63 @@
-import { MigrationInterface, QueryRunner } from 'typeorm';
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
 export class UserTableCreation1676238451353 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(
-      `CREATE TABLE IF NOT EXISTS "user" (
-        "id" UUID PRIMARY KEY,
-        "login" VARCHAR ( 50 ) NOT NULL,
-        "password" VARCHAR ( 50 ) NOT NULL,
-        "version" INTEGER NOT NULL DEFAULT 1,
-        "createdAt" BIGINT NOT NULL,
-        "updatedAt" BIGINT NOT NULL
-        );`,
+    // await queryRunner.query(
+    //   `CREATE TABLE IF NOT EXISTS "user" (
+    //     "id" UUID PRIMARY KEY,
+    //     "login" VARCHAR ( 50 ) NOT NULL,
+    //     "password" VARCHAR ( 50 ) NOT NULL,
+    //     "version" INTEGER NOT NULL DEFAULT 1,
+    //     "createdAt" BIGINT NOT NULL,
+    //     "updatedAt" BIGINT NOT NULL
+    //     );`,
+    // );
+    await queryRunner.createTable(
+      new Table({
+        name: 'user',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'uuid',
+          },
+          {
+            name: 'login',
+            type: 'varchar',
+            length: '50',
+            isNullable: false,
+          },
+          {
+            name: 'password',
+            type: 'varchar',
+            length: '50',
+            isNullable: false,
+          },
+          {
+            name: 'version',
+            type: 'integer',
+            default: 1,
+            isNullable: false,
+          },
+          {
+            name: 'createdAt',
+            type: 'bigint',
+            isNullable: false,
+          },
+          {
+            name: 'updatedAt',
+            type: 'bigint',
+            isNullable: false,
+          },
+        ],
+      }),
+      false,
     );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`DROP TABLE "user";`);
+    await queryRunner.dropTable('user');
   }
 }
