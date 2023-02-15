255de6e79c68d7e2f1b325aa6d231f2fba00e515
Vladislav Magomedov <vladdmagg94@yandex.ru>
2023-02-15 05:04:34 +0300

    refactor: move data source config to AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,30 @@
 import { UserModule } from './user/user.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { TrackModule } from './track/track.module';
 import { ArtistModule } from './artist/artist.module';
 import { AlbumModule } from './album/album.module';
 import { FavoritesModule } from './favorites/favorites.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { dataSourceOptions } from './db/data-source';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRoot(dataSourceOptions),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('POSTGRES_HOST'),
+        port: +configService.get('POSTGRES_PORT'),
+        username: configService.get('POSTGRES_USER'),
+        password: configService.get('POSTGRES_PASSWORD'),
+        database: configService.get('POSTGRES_DB'),
+        entities: ['dist/**/*.model.js'],
+        migrations: ['dist/db/migration/*.js'],
+        migrationsRun: true,
+        // synchronize: true,
+      }),
+    }),
     UserModule,
     TrackModule,
     ArtistModule,
