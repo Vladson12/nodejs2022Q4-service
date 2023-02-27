0933171ca755302114912ca660e0516442a60fe6
Vladislav Magomedov <vladdmagg94@yandex.ru>
2023-02-28 01:26:43 +0300

    fix: implement password hashing in user controller

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -8,18 +8,27 @@ import { UpdatePasswordDto } from './dto/update-password.dto';
 import { User } from './user.model';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
+import { compare, hash } from 'bcrypt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class UserService {
   constructor(
     @InjectRepository(User)
     private usersRepository: Repository<User>,
+    private configService: ConfigService,
   ) {}
 
   async create(dto: CreateUserDto): Promise<User> {
     const currentTimestamp = Date.now();
+
+    const hashedPassword = await hash(
+      dto.password,
+      +this.configService.get('CRYPT_SALT'),
+    );
     const userToCreate = new User({
       ...dto,
+      password: hashedPassword,
       version: 1,
       createdAt: currentTimestamp,
       updatedAt: currentTimestamp,
@@ -48,11 +57,14 @@ export class UserService {
       throw new NotFoundException('User with such id not found');
     }
 
-    if (!(userToUpdate.password === dto.oldPassword)) {
+    if (!(await compare(dto.oldPassword, userToUpdate.password))) {
       throw new ForbiddenException('Password wrong');
     }
 
-    userToUpdate.password = dto.newPassword;
+    userToUpdate.password = await hash(
+      dto.newPassword,
+      +this.configService.get('CRYPT_SALT'),
+    );
     userToUpdate.version++;
     userToUpdate.updatedAt = Date.now();
 
